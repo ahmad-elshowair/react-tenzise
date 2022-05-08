408916319d5cc306c618c16b30fa1d28bb099730
Ahmad Elshowair <it.is.work@outlook.com>
2022-05-08 22:59:00 +0700

    change roll btn to new game text when tenzies are tur and run confetti

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { nanoid } from "nanoid";
 import React from "react";
+import Confetti from "react-confetti";
 import { Die } from "./components/Die";
 function App() {
 
@@ -60,6 +61,7 @@ React.useEffect(()=>{
 
   return (
     <div className="main">
+      {tenzies && <Confetti/>}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
@@ -68,7 +70,7 @@ React.useEffect(()=>{
         {diceElements}
       </div>
       {tenzies && <p className="result"> you won !</p>}
-      {dice && <button className="roll--btn" onClick={rollDice}>Roll</button>}
+      {dice && <button className="roll--btn" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>}
     </div>
   );
 }
