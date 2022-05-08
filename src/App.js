43c8d8d5d584a357df272bcf0ab19a282e200dee
Ahmad Elshowair <it.is.work@outlook.com>
2022-05-08 17:56:33 +0700

    add styled roll dice button

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ function App() {
     const diceElements = dice.map(die => {
       return <Die key={die.id} value={die}/>;
     });
-
+  
+    const rollDice = () =>{
+      return setDice(allNewDice);
+    };
 
   return (
     <div className="main">
       <div className="dice--container">
         {diceElements}
       </div>
-      
+      {dice && <button className="roll--btn" onClick={rollDice}>Roll</button>}
     </div>
   );
 }
